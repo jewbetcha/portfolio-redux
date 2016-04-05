415b8bbbab839f09604a2dfa7ec2ac089e30dbe2
themezzilla <crollins199@gmail.com>
2016-04-04 23:29:50 -0500

    Couple changes to delays

--- public/js/main.js
+++ public/js/main.js
@@ -11,11 +11,11 @@
       weight: 7,         // Font weight (pixels)
       rounded: true,    // Rounded letter endings
       color: '#483D3F',  // Font color
-      duration: 0.2,       // Duration of the animation of each letter (seconds)
-      delay: 0.2,  // Delay animation among letters (seconds)
+      duration: 1,       // Duration of the animation of each letter (seconds)
+      delay: [0.1,0.4,0.2,0.45,0.15,0.35,0.1,0.2,0.1,0.3,0.3,0.1,0.2,0.14],  // Delay animation among letters (seconds)
       fade: 0.2,         // Fade effect duration (seconds)
       easing: d3_ease.easeCubicInOut.ease,   // Easing function
-      individualDelays: false,               // If false (default), every letter delay increase gradually, showing letters from left to right always. If you want to show letters in a disorderly way, set it to true, and define different delays for the desired letters.
+      individualDelays: true,               // If false (default), every letter delay increase gradually, showing letters from left to right always. If you want to show letters in a disorderly way, set it to true, and define different delays for the desired letters.
   };
 
   // Initializing the text (Letters parameters: container-element, options)
