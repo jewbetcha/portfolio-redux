0175236835f90768fdb2aec0295c789df4a2082a
themezzilla <crollins199@gmail.com>
2016-04-06 23:08:05 -0500

    Added links back in, fine tuning letters

--- public/js/main.js
+++ public/js/main.js
@@ -7,12 +7,12 @@
   // All the possible options (these are the default values)
   // Remember that every option (except individualDelays) can be defined as single value or array
   var options = {
-      size: 55,         // Font size, defined by the height of the letters (pixels)
+      size: 65,         // Font size, defined by the height of the letters (pixels)
       weight: 7,         // Font weight (pixels)
       rounded: true,    // Rounded letter endings
       color: '#483D3F',  // Font color
-      duration: 1,       // Duration of the animation of each letter (seconds)
-      delay: [0.1,0.4,0.2,0.45,0.15,0.35,0.1,0.2,0.1,0.3,0.3,0.1,0.2,0.14],  // Delay animation among letters (seconds)
+      duration: 1.2,       // Duration of the animation of each letter (seconds)
+      delay: [0.7,0.4,0.5,0.45,0.15,0.35,0.3,0.2,0.1,0.3,0.3,0.1,0.2,0.54],  // Delay animation among letters (seconds)
       fade: 0.2,         // Fade effect duration (seconds)
       easing: d3_ease.easeCubicInOut.ease,   // Easing function
       individualDelays: true,               // If false (default), every letter delay increase gradually, showing letters from left to right always. If you want to show letters in a disorderly way, set it to true, and define different delays for the desired letters.
