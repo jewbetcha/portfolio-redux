90e089eac1536e9983058aed2bae6e4d53c9c61d
themezzilla <crollins199@gmail.com>
2016-06-06 09:30:53 -0500

    Got gulp going, prefix/minify styles

---
+++ public/gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+var runSequence = require('run-sequence');
+
+gulp.task('prefix', function () {
+	return gulp.src('stylesheets/styles.css')
+		.pipe(autoprefixer({
+			browsers: ['last 3 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('./stylesheets/'));
+});
+
+gulp.task('minify', function() {
+  return gulp.src('stylesheets/styles.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename('styles.min.css'))
+    .pipe(gulp.dest('./stylesheets/'));
+});
+
+// gulp.task('watch', function() {
+//   gulp.watch('./scss/**/*.scss', ['compass']);
+//   gulp.watch('./css/styles.css', ['prefix']);
+//   gulp.watch('./js/script.js', ['compress']);
+// });
+
+gulp.task('build', function(callback) {
+  runSequence('prefix', 'minify', callback);
+});
