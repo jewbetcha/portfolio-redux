58f3cfa8a54489e742df6b450ad47a9d121e7f42
themezzilla <crollins199@gmail.com>
2016-08-18 14:40:54 -0500

    Works, added a bunch of data

--- public/fantasy-generator/script.js
+++ public/fantasy-generator/script.js
@@ -1,28 +1,25 @@
 (function($) {
   'use strict';
-  var poolOne = [
 
-  ];
-  var poolTwo = [
-
-  ];
-  var poolThree = [
-
-  ];
-
-  function nothing() {
-    return 0;
-  }
   $(document).ready(function() {
-    $('.button').click(function() {
+    var item;
+    function getRandom(arr) {
+      item = arr[Math.floor(Math.random()*arr.length)];
+      return item;
+    }
 
-      $('.result').each(function(i) {
+    var res = $('.result');
+    $('.button').click(function() {
+      res.text('');
+
+      res.each(function(i) {
         var el = $(this);
         setTimeout(function() {
-          el.addClass('fade').text('something');
+          el.addClass('fade').text(getRandom(data[i]));
         }, i * 500);
       });
 
+      res.removeClass('fade');
     });
   });
 
